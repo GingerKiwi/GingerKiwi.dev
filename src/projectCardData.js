940f771ae795be68910cb0b8fa3e39bb82036e1f
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-06-26 13:15:27 -0400

    create projectCardData.js. start adding data to the file as an array

---
+++ src/projectCardData.js
@@ -0,0 +1,58 @@
+export default [
+    {
+        projectname: "Yarn Help!",
+        problem: "Every ball of yarn has a different length, and there are with 8+ different standard thicknesses (“weights”). There are over 250,000 different yarns. Crochet uses 25% more yarn than knitting.How many balls of X yarn does a person have to buy to make Y project if they are knitting or if they are crocheting?",
+        stack: ["html", "css", "javascript", "Ravely.com API"],
+        tools: ["vscode", "git", "github"],
+        github: "https://yarnhelp.app",
+        link:"",
+        image: "/assets/yarnhelp.png",
+    },
+    {
+        projectname: "The Alice in Wonderland Open Source Project",
+        problem: "problem",
+        stack: ["html", "css", "javascript", "", ""],
+        tools: ["vscode", "git", "github"],
+        github: "",
+        link:"https://alicegame.app",
+        image: "/assets/yarnhelp.png",
+    },
+    {
+        projectname: "Ginger Kiwi Blog: JAMStack",
+        problem: "problem",
+        stack: ["HTML", "CSS", "javascript", "Prism.js", "11ty"],
+        tools: ["vscode", "git", "github"],
+        github: "",
+        link:"",
+        image: "./assets/GK-Blog-Screenshot.png",
+    },
+    {
+        projectname: "Ginger Kiwi Portfolio (this site)",
+        problem: "problem",
+        stack: ["html", "css", "javascript", "", ""],
+        tools: ["vscode", "git", "github"],
+        github: "",
+        link:"",
+        image: "/assets/",
+    },
+    {
+        projectname: "Conquering Responsive Layouts",
+        problem: "problem",
+        stack: ["HTML", "CSS", "javascript", "Prism.js"],
+        tools: ["vscode", "git", "github"],
+        github: "",
+        link:"",
+        image: "/assets/yarnhelp.png",
+    },
+]
+
+// template:
+// {
+//     projectname: "name.",
+//     problem: "problem",
+//     stack: ["html", "css", "javascript", "", ""],
+//     tools: ["vscode", "git", "github"],
+//     github: "",
+//     link:"",
+//     image: "/assets/yarnhelp.png",
+// },
