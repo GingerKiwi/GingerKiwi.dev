f523849d0476ce804e2ba683c02b0502aec61cd0
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-06-28 12:15:50 -0400

    add avatar.png via paste. add img to Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,7 @@
 const Hero = () => {
     return (
       <header>
+        <img src="./assets/avatar.png" alt="graphic of a redhead woman with a ponytail behind a laptop compute" />
         <h1>Nice to Meet You. <br></br> I'm Liz McCready</h1>
         <p className="padding-top-1rem text--align-center">Based in Toronto, Canada, I’m a front-end developer passionate about building accessible web apps and solving puzzles with others.</p>
       </header>
