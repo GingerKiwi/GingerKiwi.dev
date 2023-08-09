484c8d9fd376104b4c1cd624fe457b8b7efc7138
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-08-09 13:18:18 -0400

    fix order of img and text in hero section for lgr screens. Added text to p in hero

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,7 +7,8 @@ const Hero = () => {
         <img className="img-hero"  src={avatar} alt="graphic of a redhead woman with a ponytail behind a laptop compute" />
         <div className="header-text">
         <h1>Nice to Meet You. <br></br> I'm Liz McCready</h1>
-        <p className="padding-top-1rem text--align-center p-hero">Based in Toronto, Canada, I’m a front-end developer passionate about building accessible web apps and solving puzzles with others.</p>
+        <p className="padding-top-1rem text--align-center p-hero">Based in Toronto, Canada, I’m a front-end developer and former adaptive tech specialist passionate about building beauitful accessible web apps and solving puzzles with others.</p>
+        <p>I'm also the founder and maintainer of The Alice in Wonderland Open Source Project.</p>
         </div>
       </header>
     );
