f8acc8c46b8d4a7a614e26ab3254d2410867d4f2
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-09-18 15:20:38 -0400

    comment out group-26-orange.svg in Hero.jsx.

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,5 +1,5 @@
 import avatar from '../assets/avatar.png'
-import bgimage26 from '../assets/group-26-orange.svg'
+// import bgimage26 from '../assets/group-26-orange.svg'
 
 const Hero = () => {
     return (
@@ -7,7 +7,8 @@ const Hero = () => {
         
         
         <img className="img-hero"  src={avatar} alt="graphic of a redhead woman with a ponytail behind a laptop computer" />
-        <div className="header-text" style={{ backgroundImage:`url(${bgimage26})`,backgroundRepeat:"no-repeat"}}>
+        <div className="header-text">
+        {/* <div className="header-text" style={{ backgroundImage:`url(${bgimage26})`,backgroundRepeat:"no-repeat"}}> */}
         <h1>Nice to Meet You! <br></br> I'm Liz M<sup>c</sup>Cready</h1>
         <p className="padding-top-1rem text--align-left p-hero">Based in Toronto, Canada, I’m a front-end developer and former adaptive tech specialist passionate about building beauitful accessible web apps and solving puzzles with others.</p>
         <p>I'm also the founder and maintainer of The Alice in Wonderland Open Source Project.</p>
